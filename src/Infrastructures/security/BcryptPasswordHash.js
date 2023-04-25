31beb77c4e8b7cb9ed8f60aed604e028803fdd0b
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-04-25 19:45:59 +0700

    create encryption function and test it

---
+++ src/Infrastructures/security/BcryptPasswordHash.js
@@ -0,0 +1,15 @@
+const PasswordHash = require("../../Applications/security/PasswordHash");
+
+class BcryptPasswordHash extends PasswordHash {
+    constructor(bcrypt, saltAround = 10) {
+        super();
+        this._bcrypt = bcrypt;
+        this._saltAround = saltAround;
+    }
+
+    async hash(password) {
+        return this._bcrypt.hash(password, this._saltAround);
+    }
+}
+
+module.exports = BcryptPasswordHash;
